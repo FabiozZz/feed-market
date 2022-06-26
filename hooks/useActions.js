933bc69702637ea5a01c220f499482d96de59d1b
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2022-06-26 09:18:38 +0300

    DOC: Add description useAction hook

--- hooks/useActions.js
+++ hooks/useActions.js
@@ -2,6 +2,10 @@ import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import ActionCreators from "../store/action-creators";
 
+
+/**
+ * @description hook для объединения dispatch'а и функции санки
+ */
 export const useAction = () => {
     const dispatch = useDispatch();
     return bindActionCreators(ActionCreators, dispatch);
