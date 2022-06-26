fba7685c8c250d853cd3163a8e08f5b8101a09de
BeFunXdev <105725119+BeFunXdev@users.noreply.github.com>
2022-06-26 16:51:34 +0300

    Add checkbox

--- UI/checkbox/index.js
+++ UI/checkbox/index.js
@@ -1,13 +1,44 @@
 import classNames from 'classnames';
 import classes from './checkbox.module.scss';
 
-const Checkbox = ({id, groupChecked, ...other}) => {
+/**
+ * @description Компонент Checkbox
+ *
+ * @param {React.InputHTMLAttributes} props
+ * @property {boolean} props.groupChecked - флаг для отображения выбора всех checkbox
+ * @property {function} props.onChange - callback для события change
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+const Checkbox = ({groupChecked, onChange, ...other}) => {
+
+    let {disabled} = other;
+
+    const changeHandler = (e) => {
+        if (onChange) {
+            onChange();
+        }
+
+        let parent = e.target.parentElement;
+
+        if (parent.className.indexOf('groupChecked') !== -1) {
+            return;
+        }
+
+        if (parent.className.indexOf('achecked') !== -1) {
+            parent.className = classes.a;
+        } else {
+            parent.className = classNames(classes.a, classes.achecked);
+        }
+    }
+
     return (
         <div>
-            <input type="checkbox" id={id} className={classNames(classes.customCheckbox,
-                groupChecked ? classes.groupChecked : '')}
-                   {...other}/>
-            <label htmlFor={id}/>
+            <label className={classNames(classes.a, !disabled || classes.adisabled, groupChecked ? classes.groupChecked : '')}>
+                <input type="checkbox" className={classNames(classes.customCheckbox)}
+                       {...other} onChange={changeHandler}/>
+            </label>
         </div>
     );
 }
