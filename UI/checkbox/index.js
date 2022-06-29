ef9c0ee78485a1c3f3ab36e85893d1deb18f21c5
BeFunXdev <gj123a1@mail.ru>
2022-06-29 11:26:38 +0300

    Redesigned the radio buttons

--- UI/checkbox/index.js
+++ UI/checkbox/index.js
@@ -11,33 +11,36 @@ import classes from './checkbox.module.scss';
  * @constructor
  */
 
-const Checkbox = ({groupChecked, onChange, ...other}) => {
+const Checkbox = ({groupChecked, children, ...other}) => {
 
-    let {disabled} = other;
+    // let {disabled} = other;
 
-    const changeHandler = (e) => {
-        if (onChange) {
-            onChange();
-        }
-
-        let parent = e.target.parentElement;
-
-        if (parent.className.indexOf('groupChecked') !== -1) {
-            return;
-        }
-
-        if (parent.className.indexOf('achecked') !== -1) {
-            parent.className = classes.a;
-        } else {
-            parent.className = classNames(classes.a, classes.achecked);
-        }
-    }
+    // const changeHandler = (e) => {
+    //     if (onChange) {
+    //         onChange();
+    //     }
+    //
+    //     let parent = e.target.parentElement;
+    //
+    //     if (parent.className.indexOf('groupChecked') !== -1) {
+    //         return;
+    //     }
+    //
+    //     if (parent.className.indexOf('achecked') !== -1) {
+    //         parent.className = classes.a;
+    //     } else {
+    //         parent.className = classNames(classes.a, classes.achecked);
+    //     }
+    // }
+    //, !disabled || classes.adisabled, groupChecked ? classes.groupChecked : ''
 
     return (
         <div>
-            <label className={classNames(classes.a, !disabled || classes.adisabled, groupChecked ? classes.groupChecked : '')}>
+            <label>
                 <input type="checkbox" className={classNames(classes.customCheckbox)}
-                       {...other} onChange={changeHandler}/>
+                       {...other}/>
+                <span className={classes.view}/>
+                <span>{children}</span>
             </label>
         </div>
     );
