851da2648c1467d4c7bca59c30ae3d3adb45ac8b
FabiozZzDev <fabiozzz.dev@gmail.com>
2022-06-29 14:14:33 +0300

    CHORE: Replace logic working Component header navigation

--- components/common/layouts/index.jsx
+++ components/common/layouts/index.jsx
@@ -1,7 +1,31 @@
 import NavBar from 'components/common/layouts/nav-bar';
-import Footer from 'components/common/layouts/footer';
-import { useEffect, useRef, useState } from 'react';
-import { func } from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import Slide from "@mui/material/Slide";
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import {AppBar} from "@mui/material";
+
+/**
+ *
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const  HideOnScroll = ({children})=> {
+
+	const [scrollTarget,setScrollTarget] = useState(undefined)
+
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			setScrollTarget(document.getElementById('__next'))
+		}
+	},[]);
+	const trigger = useScrollTrigger({target:scrollTarget});
+	return (
+		<Slide appear={false} direction="down" in={!trigger}>
+			{children}
+		</Slide>
+	);
+}
 
 /**
  * @description Корневой Layout, содержит основную разметку страницы, одинаковую для нескольких страниц
@@ -24,20 +48,25 @@ import { func } from 'prop-types';
  * @constructor
  */
 const Layout = ({ children }) => {
-	const [navbar, setNavbar] = useState(true);
 
-	function scroll(e) {
-		if (e.deltaY < 1) {
-			setNavbar(true);
-		} else if (e.deltaY > 1) {
-			setNavbar(false);
-		}
-	}
+	/**
+	 * @type {React.MutableRefObject<HTMLDivElement | null>}
+	 */
 
 	return (
-		<div onWheel={scroll} className={'app'}>
-			<NavBar show={navbar} />
-			<main className='app__content'>{children}</main>
+		<div className={'app'}>
+			<HideOnScroll>
+				<AppBar color={'transparent'} sx={{
+					'&':{
+						boxShadow:'none'
+					}
+				}}>
+					{/*<Toolbar>*/}
+					  <NavBar />
+					{/*</Toolbar>*/}
+				</AppBar>
+			</HideOnScroll>
+			<main  className='app__content'>{children}</main>
 		</div>
 	);
 };
