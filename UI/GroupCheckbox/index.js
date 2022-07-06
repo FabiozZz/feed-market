1071418b236e48c99fc9fcfbb7b3c0a04fa6af3c
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2022-07-06 12:42:55 +0300

    CHORE: GroupCheckbox from branch GroupCheckbox

--- UI/GroupCheckbox/index.js
+++ UI/GroupCheckbox/index.js
@@ -1,139 +1,112 @@
-<<<<<<< HEAD
-import classNames from "classnames";
-=======
->>>>>>> master
-import {cloneElement, useEffect, useState} from "react";
+import { cloneElement, useEffect, useState } from 'react';
 
-const GroupCheckbox = ({children, onClick, ...other}) => {
+/**
+ * @description Компонент GroupCheckbox
+ *
+ * @param {React.InputHTMLAttributes} props
+ * @returns {JSX.Element}
+ * @constructor
+ */
 
-    let [content, setContent] = useState(children);
-    let [checkboxes, setCheckboxes] = useState([]);
-    useEffect(() => {
-<<<<<<< HEAD
-        let checkbox = [];
-        content?.forEach((item, index) => {
-            if (item?.type?.name === "Checkbox")
-                checkbox.push(index);
-        });
-        let buf = content?.map((item, index) => {
-            if (checkbox.includes(index)) {
-                if (index === 0) {
-                    return cloneElement(item, {...item.props, 'data-maingroup': true, key: index})
-                } else {
-                    return cloneElement(item, {...item.props, key: index})
-                }
-            } else {
-                return item
-            }
-        });
-        setContent(buf);
-        setCheckboxes(checkbox)
-    }, []);
+const GroupCheckbox = ({ children, onClick, ...other }) => {
+	let [content, setContent] = useState(children);
+	let [checkboxes, setCheckboxes] = useState([]);
+	useEffect(() => {
+		if (
+			!Array.isArray(content) ||
+			content.filter(item => item?.type?.name === 'Checkbox').length === 0
+		) {
+			throw new Error("Внутри необходимы checkbox'ы");
+		} else if (content.filter(item => item?.type?.name === 'Checkbox').length === 1) {
+			throw new Error("Внутри должно быть от двух checkbox'сов");
+		}
 
-    const clearCheckBox = (groupChecked) => {
-        setContent('')
-        let a = content.map((item, index) => {
-            if (checkboxes.includes(index)) {
-                if (index !== checkboxes[0]) {
-                    console.log(item)
-                    return cloneElement(item, {...item.props, checked: false, groupChecked: groupChecked});
-                } else {
-                    return item;
-=======
-        if (!Array.isArray(content) || content.filter(item => item?.type?.name === "Checkbox").length === 0) {
-            throw new Error('Внутри необходимы checkbox\'ы')
-        } else if (content.filter(item => item?.type?.name === "Checkbox").length === 1) {
-            throw new Error('Внутри должно быть от двух checkbox\'сов')
-        }
+		let checkbox = [];
+		setContent(
+			content?.map((item, index) => {
+				if (item?.type?.name === 'Checkbox') {
+					checkbox.push({ index, checked: item.props.checked || false });
+					if (index === 0) {
+						return cloneElement(item, {
+							...item.props,
+							'data-id': index,
+							'data-maingroup': true,
+							key: index,
+						});
+					} else {
+						return cloneElement(item, { ...item.props, 'data-id': index, key: index });
+					}
+				} else {
+					return item;
+				}
+			})
+		);
+		setCheckboxes(checkbox);
+		//eslint-disable-next-line
+	}, []);
 
-        let checkbox = [];
-        setContent(content?.map((item, index) => {
-            if (item?.type?.name === "Checkbox") {
-                checkbox.push({index, checked: item.props.checked || false});
-                if (index === 0) {
-                    return cloneElement(item, {...item.props, 'data-id': index, 'data-maingroup': true, key: index});
-                } else {
-                    return cloneElement(item, {...item.props, 'data-id': index, key: index});
->>>>>>> master
-                }
-            } else {
-                return item
-            }
-<<<<<<< HEAD
-        });
-        setContent(a);
-    };
+	const clearCheckBox = (array, groupChecked, isClear) => {
+		let changeContent = Array.from(array);
+		checkboxes.forEach((item, i) => {
+			let element = changeContent[item.index];
+			let check = !isClear ? false : element.props.checked;
+			return i !== 0
+				? (changeContent[item.index] = cloneElement(element, {
+						...element.props,
+						checked: check,
+						groupChecked: groupChecked,
+				  }))
+				: !isClear
+				? element
+				: (changeContent[item.index] = cloneElement(element, {
+						...element.props,
+						checked: false,
+				  }));
+		});
+		setContent(changeContent);
+	};
 
-=======
-        }));
-        setCheckboxes(checkbox)
-    }, []);
+	const changeCheck = (index, check) => {
+		checkboxes.forEach(item => {
+			if (item.index === +index) {
+				item.checked = !check;
+			}
+		});
+		return content.map((item, i) => {
+			if (i === +index) {
+				return cloneElement(item, { ...item.props, checked: !check });
+			} else {
+				return item;
+			}
+		});
+	};
 
-    const clearCheckBox = (array, groupChecked, isClear) => {
-        let changeContent = Array.from(array);
-        checkboxes.forEach((item, i) => {
-            let element = changeContent[item.index];
-            let check = !isClear ? false : element.props.checked;
-            return i !== 0 ? changeContent[item.index] = cloneElement(element, {...element.props, checked: check, groupChecked: groupChecked}) :
-                !isClear ? element : changeContent[item.index] = cloneElement(element, {...element.props, checked: false});
-        })
-        setContent(changeContent);
-    };
+	const ClickHandler = e => {
+		if (onClick) {
+			onClick(e);
+		}
 
-    const changeCheck = (index, check) => {
-        checkboxes.forEach(item => {
-            if (item.index === +index) {
-                item.checked = !check;
-            }
-        });
-        return content.map((item, i) => {
-            if(i === +index) {
-                return cloneElement(item, {...item.props, checked: !check})
-            } else {
-                return item;
-            }
-        });
-    }
+		let wrap = e.target.closest('label');
+		if (wrap?.children[0].tagName === 'INPUT') {
+			let input = wrap.children[0];
+			let array = changeCheck(input.dataset.id, input.checked);
+			if (input.dataset.maingroup === 'true') {
+				if (input.checked) {
+					clearCheckBox(array, false);
+				} else {
+					clearCheckBox(array, true);
+				}
+			} else {
+				clearCheckBox(array, false, true);
+			}
+		}
+	};
 
->>>>>>> master
-    const ClickHandler = (e) => {
-        if (onClick) {
-            onClick(e);
-        }
-
-        let wrap = e.target.closest('label');
-        if (wrap?.children[0].tagName === "INPUT") {
-<<<<<<< HEAD
-            let input = wrap.children[0]
-            if (input.dataset.maingroup === 'true') {
-                if (input.checked) {
-                    clearCheckBox(true);
-                } else {
-                    clearCheckBox(false);
-                }
-            } else {
-                clearCheckBox(false);
-=======
-            let input = wrap.children[0];
-            let array = changeCheck(input.dataset.id, input.checked);
-            if (input.dataset.maingroup === 'true') {
-                if (input.checked) {
-                    clearCheckBox(array,false);
-                } else {
-                    clearCheckBox(array,true);
-                }
-            } else {
-                clearCheckBox(array,false, true);
->>>>>>> master
-            }
-        }
-    }
-
-  return(
-      <div {...other} onClick={ClickHandler}>
-          {content}
-      </div>
-  );
-}
+	return (
+		<div {...other} onClick={ClickHandler}>
+			{content}
+		</div>
+	);
+};
 
 export default GroupCheckbox;
